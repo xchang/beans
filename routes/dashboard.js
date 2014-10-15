0761acdf650e0bc3e66f34e1c80e03d675090de3
xin.chang <xin.chang@autonaiv.com>
2014-10-15 14:35:52 +0800

    persist projects with mongodb.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var fetchProjects = function(db, callback) {
+  var collection = db.collection('projects');
+  collection.find({}).toArray(function(err, docs) {
+    console.log("Found the following records");
+    console.dir(docs)
+    callback(docs);
+  });
+}
+
 router.get('/', function(req, res) {
-  res.render('dashboard');
+  var url = 'mongodb://localhost:27017/beans_dev';
+  var mongoClient = require('mongodb').MongoClient;
+
+  mongoClient.connect(url, function(err, db) {
+    console.log("Connected correctly to server");
+
+    fetchProjects(db, function(projects){
+      res.render('dashboard', {projects: projects});
+      db.close();
+    });
+  });
 });
 
 module.exports = router;
